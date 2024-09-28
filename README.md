95ca98b518e2ac627a614130648c00df74c75fff
Lislayne <lis.figueira@agenciafg.com.br>
2024-09-28 00:14:10 -0300

    desenvolvedor-cadastra

--- README.md
+++ README.md
@@ -1,76 +1,37 @@
-# Venha ser um desenvolvedor da Cadastra
 
-Olá! Somos a Cadastra, uma Next-Gen Company que oferece soluções globais em marketing, tecnologia, estratégia de negócios, commerce, dados e analytics. Este desafio é voltado para o nosso Centro de Excelência (COE) de Commerce, especializado em implantação, migração, evolução e otimização de e-commerce.  Contamos com escritórios em 5 países, mas num formato de trabalho 100% home office.
+</p>
 
-Estamos em busca de Pessoas Desenvolvedoras `Front-end`, com sólidos conhecimentos em HTML, CSS e TS para projetar interfaces e entregar a melhor experiência para os consumidores visando sempre a otimização do código e agilidade no desenvolvimento.
+<h4 align="center"> 
+	Desenvolvedor Cadastra
+</h4>
 
-## O que procuramos
 
-### Requisitos Obrigatórios
+## 💻 Sobre o projeto
 
-- Html5, css3;
-- Javascript/Typescript;
-- Consumo de APIs.
-- Versionamento Git;
-- Grunt/gulp;
-- Sass/less;
-- Nodejs
-- Sites responsivos;
-- Iniciativa, criatividade e colaboração;
-- Boas práticas: reutilização de código, semântica, organização, performance.
+O projeto desenvolvido faz parte de um teste para vaga de Desenvolvedor Front-End na Cadastra, onde o objetivo é recriar parte de um layout proposto.
 
-### Desejáveis:
+---
 
-- Experiência com algum CMS
-- Conhecimento/Experiência em Vtex
-- Nodejs
+## ⚙️ Funcionalidades
 
-----
+- [x] Filtro de produtos por preço.
+- [x] Filtro de produtos por cor.
+- [x] Filtro de produtos por tamannho.
+- [x] Adição de produtos ao carrinho.
+- [x] Carregar mais produtos.
+- [x] Filtro de produtos mais recente, maior preço e menor preço
 
-## Como se candidatar a vaga
+---
 
-- [Crie um fork desse projeto;](https://github.com/Cadastra-Next-Gen-Company/desenvolvedor-cadastra/fork)
-- No seu fork, codifique o layout seguindo as instruções abaixo;
-- Atualize o readme com as instruções necessárias para rodar o seu código;
-- Adicione também seu e-mail de contato ao readme do projeto ( caso seu e-mail do github esteja privado )
-- envie para [vinicius.delfino@cadastra.com](mailto:vinicius.delfino@cadastra.com?subject=Vaga%20DEV%20-%20Cadastra) com cópia para [andresa.klein@cadastra.com](mailto:andresa.klein?subject=Vaga%20DEV%20-%20Cadastra)
- 
+## 🚀 Como executar o projeto
 
-**obs.**: link do projeto [github.com/Cadastra-Next-Gen-Company/desenvolvedor-cadastra](https://github.com/Cadastra-Next-Gen-Company/desenvolvedor-cadastra)
+Com o node instalado, inicie um projeto com `npm start`.
 
-### Instruções para o teste
+---
 
-O layout se encontra no [figma](https://www.figma.com/file/Z5RCG3Ewzwm7XIPuhMUsBZ/Desafio-Cadastra?type=design&node-id=0%3A1&mode=design&t=A0G2fRjMSrcQjchw-1).
+## Dados de contato
 
-### Dependências
+- [Linkedin](https://www.linkedin.com/in/lislayne-figueira-carr%C3%ADlio-2b4a721b5/) 
+- Email [lislaynefigueira@gmail.com](mailto:lislaynefigueira@gmail.com)
 
-O projeto possui um setup pronto no qual há a necessidade de possuir o nodejs instalado na versão 14 ou superior.
-
-Para instalar as dependências só é preciso executar o comando: `npm install`
-
-O dar start no server e nos processos para desenvolvimento é necessário rodar o comando: `npm start `
-
-Uma ver que o comando é dado ele irá levantar 2 servidores, sendo eles:
- - um para acessar o front-end que roda na porta 3000. No qual pode ser acessado pela url: http://localhost:3000
- - um para o json-server que irá export uma api com a lista de produtos que roda na porta 5000. Para acessar os produtos é na url:  http://localhost:5000/products
-
-### Objetivo
-
-O objetivo desse desafio é avaliar a seus conhecimentos fundamentais de front end, por isso pedimos que não utilize nenhum framework, porem caso deseje utilizar frameworks como react, é possível adicionar ao setup.
-### Obrigatório
-
-- HTML5 e CSS3
-- Typescript
-- Requisição a API para obter os produtos
-- Funcionalidade: Filtrar produtos por cor, tamanho e preço.
-- Funcionalidade: Adicionar produto ao carrinho.
-- Funcionalidade: Carregar mais produtos.
-- Não utilizar Bootstrap, Foundation Css, Semantic ui ou semelhantes ;
-- Responsividade
-
-#### Bônus
-
-- Typescript com clean code
-- Adição de processo no gulp para otimizar imagens
-
-##### Dúvidas: [vinicius.delfino@cadastra.com](mailto:vinicius.delfino@cadastra.com?subject=Dúvida%20Vaga%20DEV%20-%20Cadastra)
+---
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,334 @@
 import { Product } from "./Product";
 
-const serverUrl = "http://localhost:5000";
+const serverUrl = "http://localhost:5005";
 
 function main() {
   console.log(serverUrl);
 }
 
 document.addEventListener("DOMContentLoaded", main);
+
+let products: Product[] = [];
+let filteredProducts: Product[] = [];
+let visibleProductsCount = 8;
+let cartCount = 0;
+
+const updateCartCount = () => {
+  const countDisplay = document.querySelector('.count') as HTMLElement;
+  cartCount += 1;
+  countDisplay.textContent = String(cartCount);
+};
+
+const fetchProducts = async () => {
+  try {
+    const response = await fetch('http://localhost:5005/products');
+    const data = await response.json();
+    products = data;
+    filteredProducts = products;
+    renderProducts(filteredProducts);
+  } catch (error) {
+    console.error('Erro ao buscar produtos:', error);
+  }
+};
+
+const renderProducts = (productsToRender: Product[]) => {
+  const productContainer = document.getElementById('products');
+  const loadMoreButton = document.querySelector('.product-list__load-more') as HTMLButtonElement;
+
+  if (productContainer) {
+    productContainer.innerHTML = '';
+
+    if (productsToRender.length === 0) {
+      productContainer.innerHTML = `<p class="no-products">Não há produtos com o filtro selecionado.</p>`;
+      loadMoreButton.style.display = 'none';
+      return;
+    }
+
+    productsToRender.slice(0, visibleProductsCount).forEach((product) => {
+      const formattedPrice = product.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+      const formattedParcelamento = Number(product.parcelamento[1]).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      });
+
+      const productCard = `
+        <div class="product-card">
+          <img class="image" src="${product.image}" alt="${product.name}" />
+          <h3>${product.name}</h3>
+          <p class="price">${formattedPrice}</p>
+          <p class="parcelamento">até ${product.parcelamento[0]}x de ${formattedParcelamento}</p>
+          <p class="size" style="display: none;">${product.size}</p>
+          <p class="color" style="display: none;">${product.color}</p>
+          <button data-id="${product.id}" class="add-to-cart">Comprar</button>
+        </div>
+      `;
+      productContainer.innerHTML += productCard;
+    });
+
+    if (productsToRender.length > visibleProductsCount) {
+      loadMoreButton.style.display = 'block';
+    } else {
+      loadMoreButton.style.display = 'none';
+    }
+
+    addAddToCartEventListeners();
+  }
+};
+
+
+const addAddToCartEventListeners = () => {
+  const addToCartButtons = document.querySelectorAll('.add-to-cart') as NodeListOf<HTMLButtonElement>;
+
+  addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      console.log('Produto adicionado ao carrinho');
+      showPopup();
+      updateCartCount();
+    });
+  });
+};
+
+const showPopup = () => {
+  const popup = document.getElementById('popup-add-cart') as HTMLElement;
+  popup.style.display = 'block';
+
+  setTimeout(() => {
+    popup.style.display = 'none';
+  }, 3000);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const closePopupBtn = document.getElementById('close-popup') as HTMLButtonElement;
+
+  closePopupBtn.addEventListener('click', () => {
+    const popup = document.getElementById('popup-add-cart') as HTMLElement;
+    popup.style.display = 'none';
+  });
+});
+
+const filterProducts = () => {
+  const selectedColors = Array.from(
+    document.querySelectorAll("input[type='checkbox']:checked")
+  ).filter((checkbox: any) => checkbox.id.startsWith('amarelo') || checkbox.id.startsWith('azul') || checkbox.id.startsWith('branco') || checkbox.id.startsWith('cinza') || checkbox.id.startsWith('laranja') || checkbox.id.startsWith('verde') || checkbox.id.startsWith('vermelho') || checkbox.id.startsWith('preto'))
+    .map((checkbox: any) => checkbox.id);
+
+  const selectedSizes = Array.from(
+    document.querySelectorAll("input[type='checkbox']:checked")
+  ).filter((checkbox: any) => checkbox.id.startsWith('tamanho'))
+    .map((checkbox: any) => checkbox.id.replace('tamanho-', '').toUpperCase());
+
+  const selectedPrices = Array.from(
+    document.querySelectorAll("input[type='checkbox']:checked")
+  ).filter((checkbox: any) => checkbox.id.startsWith('preco'))
+    .map((checkbox: any) => checkbox.id);
+
+  filteredProducts = products.filter((product) => {
+    // Filtra por cor
+    const colorMatch = selectedColors.length
+      ? selectedColors.includes(product.color.toLowerCase())
+      : true;
+
+    // Filtra por tamanho
+    const sizeMatch = selectedSizes.length
+      ? selectedSizes.some(size => product.size.includes(size.toUpperCase()))
+      : true;
+
+    // Filtra por preço
+    const priceMatch = selectedPrices.length ? selectedPrices.some((priceRange) => {
+      switch (priceRange) {
+        case 'preco-0-50':
+          return product.price >= 0 && product.price <= 50;
+        case 'preco-51-150':
+          return product.price >= 51 && product.price <= 150;
+        case 'preco-151-300':
+          return product.price >= 151 && product.price <= 300;
+        case 'preco-301-500':
+          return product.price >= 301 && product.price <= 500;
+        case 'preco-500':
+          return product.price > 500;
+        default:
+          return true;
+      }
+    }) : true;
+
+    return colorMatch && sizeMatch && priceMatch;
+  });
+
+  visibleProductsCount = 8;
+  renderProducts(filteredProducts);
+};
+
+
+const loadMoreProducts = () => {
+  visibleProductsCount += 8;
+  renderProducts(filteredProducts);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchProducts();
+
+  document.querySelectorAll("input[type='checkbox']").forEach((checkbox) => {
+    checkbox.addEventListener("change", filterProducts);
+  });
+
+  const loadMoreButton = document.querySelector('.product-list__load-more') as HTMLButtonElement;
+  loadMoreButton.addEventListener('click', loadMoreProducts);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const selectTrigger = document.querySelector('.select-trigger') as HTMLDivElement;
+  const options = document.querySelectorAll('.custom-option') as NodeListOf<HTMLLIElement>;
+  const optionsContainer = document.querySelector('.custom-options') as HTMLUListElement;
+
+  selectTrigger.addEventListener('click', () => {
+    optionsContainer.classList.toggle('open');
+  });
+
+  options.forEach(option => {
+    option.addEventListener('click', () => {
+      options.forEach(opt => opt.classList.remove('selected'));
+      option.classList.add('selected');
+      selectTrigger.textContent = option.textContent;
+      optionsContainer.classList.remove('open');
+
+      const selectedValue = option.getAttribute('data-value');
+      console.log(`Ordenar por: ${selectedValue}`);
+    });
+  });
+
+  document.addEventListener('click', (e) => {
+    const target = e.target as HTMLElement;
+    if (!target.closest('.custom-select')) {
+      optionsContainer.classList.remove('open');
+    }
+  });
+});
+
+
+
+
+//popup adicionar ao carrinho
+document.addEventListener('DOMContentLoaded', () => {
+  const popup = document.getElementById('popup-add-cart') as HTMLElement;
+  const closePopupBtn = document.getElementById('close-popup') as HTMLButtonElement;
+
+  function showPopup() {
+    popup.style.display = 'block';
+
+    setTimeout(() => {
+      popup.style.display = 'none';
+    }, 3000);
+  }
+
+  closePopupBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const orderBtn = document.querySelector('.filter-mobile .order') as HTMLElement;
+  const orderPopup = document.getElementById('order-popup') as HTMLElement;
+  const closePopupBtn = document.getElementById('close-order-popup') as HTMLElement;
+
+  if (orderBtn && orderPopup && closePopupBtn) {
+    orderBtn.addEventListener('click', () => {
+      orderPopup.style.display = 'flex';
+    });
+
+    closePopupBtn.addEventListener('click', () => {
+      orderPopup.style.display = 'none';
+    });
+
+    orderPopup.addEventListener('click', (event: MouseEvent) => {
+      if (event.target === orderPopup) {
+        orderPopup.style.display = 'none';
+      }
+    });
+  } else {
+    console.error('Elementos necessários não encontrados no DOM');
+  }
+});
+
+//filter mobile
+document.addEventListener('DOMContentLoaded', () => {
+  const filterBtn = document.querySelector('.filter-mobile .filter') as HTMLElement;
+  const filterPopup = document.getElementById('filter-popup') as HTMLElement;
+  const closeFilterPopupBtn = document.getElementById('close-filter-popup') as HTMLElement;
+  const applyFiltersBtn = document.querySelector('.apply-filters') as HTMLElement;
+
+  if (filterBtn && filterPopup && closeFilterPopupBtn && applyFiltersBtn) {
+    filterBtn.addEventListener('click', () => {
+      filterPopup.style.display = 'flex';
+    });
+
+    closeFilterPopupBtn.addEventListener('click', () => {
+      filterPopup.style.display = 'none';
+    });
+
+    filterPopup.addEventListener('click', (event: MouseEvent) => {
+      if (event.target === filterPopup) {
+        filterPopup.style.display = 'none';
+      }
+    });
+
+    applyFiltersBtn.addEventListener('click', () => {
+      filterPopup.style.display = 'none';
+    });
+  } else {
+    console.error('Elementos do filtro não encontrados no DOM');
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const filterCategories = document.querySelectorAll('.filter-category') as NodeListOf<HTMLElement>;
+
+  filterCategories.forEach(category => {
+    const title = category.querySelector('.title') as HTMLElement;
+
+    title.addEventListener('click', () => {
+      const filterList = category.querySelector('.filter-open') as HTMLElement;
+
+      if (filterList.style.display === 'none' || filterList.style.display === '') {
+        filterList.style.display = 'block';
+        filterList.classList.add('ativo');
+        filterList.classList.remove('inativo');
+      } else {
+        filterList.style.display = 'none';
+        filterList.classList.add('inativo');
+        filterList.classList.remove('ativo');
+      }
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const orderPopup = document.getElementById('order-popup') as HTMLElement;
+  const closePopupBtn = document.getElementById('close-order-popup') as HTMLElement;
+  const orderOptions = document.querySelectorAll('.order-option') as NodeListOf<HTMLElement>;
+
+  const sortProducts = (sortType: string) => {
+    if (sortType === 'Maior preço') {
+      filteredProducts.sort((a, b) => b.price - a.price);
+    } else if (sortType === 'Menor preço') {
+      filteredProducts.sort((a, b) => a.price - b.price);
+    } else {
+
+    }
+
+    renderProducts(filteredProducts);
+  };
+
+  closePopupBtn.addEventListener('click', () => {
+    orderPopup.style.display = 'none';
+  });
+
+  orderOptions.forEach(option => {
+    option.addEventListener('click', () => {
+      const sortType = option.textContent;
+      sortProducts(sortType || '');
+      orderPopup.style.display = 'none';
+    });
+  });
+});
+
